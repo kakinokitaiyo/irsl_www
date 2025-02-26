52d4649a6861b947b56ddd3d8665507cb5c0d509
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2025-02-26 12:40:07 +0900

    update

--- docker/build_integration.sh
+++ docker/build_integration.sh
@@ -4,7 +4,8 @@ _COMMON_NAME=${COMMON_NAME:-"eiiris.tut.ac.jp"}
 _BASE_IMAGE=${1:-"ros:noetic-ros-base"}
 _OUTPUT_IMAGE=${OUTPUT:-"apache_rosbridge_win:noetic"}
 
-docker build . --pull -f Dockerfile.apache_rosbridge --build-arg BASE_IMAGE=${_BASE_IMAGE} -t temp_build:apache0
+echo "docker build . --no-cache --pull -f Dockerfile.apache_rosbridge --build-arg BASE_IMAGE=${_BASE_IMAGE} -t temp_build:apache0"
+docker build . --no-cache --pull -f Dockerfile.apache_rosbridge --build-arg BASE_IMAGE=${_BASE_IMAGE} -t temp_build:apache0
 
 docker build ..       -f Dockerfile.add_files -t  ${_OUTPUT_IMAGE} \
        --build-arg BASE_IMAGE=temp_build:apache0 \
