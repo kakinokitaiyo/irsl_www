4a4d935a4568164a965c5044e7ab447c4c4db080
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2025-07-24 13:14:36 +0900

    update for bulding with catkin as a package

--- docker/build.sh
+++ docker/build.sh
@@ -1,15 +1,67 @@
 #!/bin/bash
 
-_COMMON_NAME=${1:-"eiiris.tut.ac.jp"}
+abs_dir=$(dirname "$(readlink -f "$0")")
 
-if [ ! -e userdir/server.key ]; then
-    openssl genrsa -out userdir/server.key 2048
+_COMMON_NAME="eiiris.tut.ac.jp"
+
+_REPO="repo.irsl.eiiris.tut.ac.jp/"
+_TAG="noetic"
+_KEYS=""
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --common-name)
+            _COMMON_NAME="$2"
+            shift
+            shift
+            ;;
+        --repo)
+            _REPO="$2"
+            shift
+            shift
+            ;;
+        --tag)
+            _TAG="$2"
+            shift
+            shift
+            ;;
+        --just-keys)
+            _KEYS="yes"
+            shift
+            ;;
+        --help)
+            echo "Usage: build.sh [--common-name COMMON_NAME] [--repo REPO] [--tag TAG] [--just-keys]"
+            echo ""
+            echo "Options:"
+            echo "  --common-name COMMON_NAME   Set the common name for SSL certificate generation (default: eiiris.tut.ac.jp)."
+            echo "  --repo REPO                 Specify the Docker repository (default: repo.irsl.eiiris.tut.ac.jp/)."
+            echo "  --tag TAG                   Specify the Docker image tag (default: noetic)."
+            echo "  --just-keys                 Only generate SSL keys and exit."
+            exit 0
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+if [ ! -e ${abs_dir}/userdir/server.key ]; then
+    openssl genrsa -out ${abs_dir}/userdir/server.key 2048
 fi
 
-if [ ! -e userdir/server.crt ]; then
-    openssl req -out userdir/server.csr -key userdir/server.key -new -subj "/C=JP/ST=Aichi/O=IRSL/CN=${_COMMON_NAME}"
-    openssl x509 -req -days 3650 -signkey userdir/server.key -in userdir/server.csr -out userdir/server.crt
+if [ ! -e ${abs_dir}/userdir/server.crt ]; then
+    openssl req -out ${abs_dir}/userdir/server.csr -key ${abs_dir}/userdir/server.key -new -subj "/C=JP/ST=Aichi/O=IRSL/CN=${_COMMON_NAME}"
+    openssl x509 -req -days 3650 -signkey ${abs_dir}/userdir/server.key -in ${abs_dir}/userdir/server.csr -out ${abs_dir}/userdir/server.crt
 fi
 
-docker build . --pull -f Dockerfile.apache_rosbridge --build-arg BASE_IMAGE=ros:noetic-ros-base -t apache_rosbridge:noetic
-#docker build . --pull -f Dockerfile.apache_rosbridge --build-arg BASE_IMAGE=repo.irsl.eiiris.tut.ac.jp/irsl_base:one_opengl -t apache_rosbridge:one
+if [ -n "${_KEYS}" ]; then
+    exit 0
+fi
+
+if   [ "${_TAG}" == "noetic" ]; then
+(cd ${abs_dir}; \
+docker build . --pull -f Dockerfile.apache_rosbridge --build-arg BASE_IMAGE=ros:noetic-ros-base -t ${_REPO}apache_rosbridge:noetic ; )
+elif [ "${_TAG}" == "one"    ]; then
+(cd ${abs_dir}; \
+docker build . --pull -f Dockerfile.apache_rosbridge --build-arg BASE_IMAGE=${_REPO}irsl_base:one_opengl -t ${_REPO}apache_rosbridge:one ; )
+fi
